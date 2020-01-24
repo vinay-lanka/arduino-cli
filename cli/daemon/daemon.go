d998ac7a718aed47da0286646df308d4cce92ee6
Roberto Sora <r.sora@arduino.cc>
2020-01-24 16:46:13 +0100

    Refactor the telemetry module

--- cli/daemon/daemon.go
+++ cli/daemon/daemon.go
@@ -29,8 +29,7 @@ import (
 	srv_commands "github.com/arduino/arduino-cli/rpc/commands"
 	srv_monitor "github.com/arduino/arduino-cli/rpc/monitor"
 	srv_settings "github.com/arduino/arduino-cli/rpc/settings"
-	stats "github.com/segmentio/stats/v4"
-	"github.com/segmentio/stats/v4/prometheus"
+	"github.com/arduino/arduino-cli/telemetry"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -57,22 +56,8 @@ var daemonize bool
 
 func runDaemonCommand(cmd *cobra.Command, args []string) {
 
-	// Configure telemetry engine
-	// Create a Prometheus default handler
-	ph := prometheus.DefaultHandler
-	// Replace the default stats engine with an engine that prepends the "daemon" prefix to all metrics
-	stats.DefaultEngine = stats.WithPrefix("daemon")
-	// Register the handler so it receives metrics from the default engine.
-	stats.Register(ph)
-	// Flush the default stats engine on return to ensure all buffered
-	// metrics are sent to the server.
-	defer stats.Flush()
-	// move everything inside commands and search for setting up a common prefix for all metrics sent!
-	logrus.Infof("Setting up Prometheus telemetry on /metrics, TCP port 2112")
-	go func() {
-		http.Handle("/metrics", ph)
-		logrus.Error(http.ListenAndServe(":2112", nil))
-	}()
+	telemetry.Activate("daemon")
+	defer telemetry.Engine.Flush()
 
 	port := viper.GetString("daemon.port")
 	s := grpc.NewServer()
@@ -106,6 +91,7 @@ func runDaemonCommand(cmd *cobra.Command, args []string) {
 		go func() {
 			// stdin is closed when the controlling parent process ends
 			_, _ = io.Copy(ioutil.Discard, os.Stdin)
+			telemetry.Engine.Flush()
 			os.Exit(0)
 		}()
 	}
@@ -118,5 +104,4 @@ func runDaemonCommand(cmd *cobra.Command, args []string) {
 	if err := s.Serve(lis); err != nil {
 		logrus.Fatalf("failed to serve: %v", err)
 	}
-
 }
